d89be83c60fc5a8c2d9a2a403a5bff7ad38227b7
Navaneeth <guraninavaneeth@gmail.com>
2026-06-14 20:32:58 +0530

    Change BrowserRouter to HashRouter in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import { AppProvider, useApp } from './context/AppContext'
 
