43e9f6ed8e6f96dfdcd09acd5a28e97d2325b296
Navaneeth Mahesh <guraninavaneeth@gmail.com>
2026-06-15 10:00:44 +0530

    fixed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/Task-Manager/',
   plugins: [react()],
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
